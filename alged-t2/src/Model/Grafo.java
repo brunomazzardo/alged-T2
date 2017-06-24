201b3c18f2044577f958896fb5cad617b067f13c
Ricardo Borges da Silva <ricardo.silva.009@acad.pucrs.br>
2017-06-24 20:54:59 -0300

    Organizando class Main e Grafo

--- alged-t2/src/Model/Grafo.java
+++ alged-t2/src/Model/Grafo.java
@@ -24,12 +24,28 @@ public class Grafo {
 	ArrayList<Aeroporto> lv = new ArrayList<Aeroporto>(); 
 	
 	static class Node {
-		Rota rota;
-		Aeroporto aeroporto;
+		private Rota rota;
+		private Aeroporto aeroporto;
 	
 		public Node(Rota rota, Aeroporto aeroporto) {
 			super();
 			this.rota = rota;
+			this.aeroporto = aeroporto;
+		}
+
+		public Rota getRota() {
+			return rota;
+		}
+
+		public void setRota(Rota rota) {
+			this.rota = rota;
+		}
+
+		public Aeroporto getAeroporto() {
+			return aeroporto;
+		}
+
+		public void setAeroporto(Aeroporto aeroporto) {
 			this.aeroporto = aeroporto;
 		}
 
@@ -39,7 +55,10 @@ public class Grafo {
 		}
 	}
 	
-	//public Grafo(ArrayList<Rota> rotas, ArrayList<Aeroporto> aeroportos){
+	/**
+	 * Popula la (lista de adjacencia)
+	 * @param rotas
+	 */
 	public Grafo(ArrayList<Rota> rotas){
 		for(int i=0; i<rotas.size(); i++){
 			Aeroporto origem = rotas.get(i).getOrigem();
@@ -52,6 +71,11 @@ public class Grafo {
 		}
 	}
 	
+	/**
+	 * Encontra o indice de um aeroporto na lv (lista de vértices)
+	 * @param codigo
+	 * @return
+	 */
 	public int findAirportIndex(String codigo){
 		for(int i=0; i<lv.size(); i++){
 			if((lv.size()-1 <= i) && lv.get(i).getCodigo().equals(codigo)){
@@ -61,18 +85,31 @@ public class Grafo {
 		return -1;
 	}
 
+	/**
+	 * Adiciona nodos na L.A
+	 * @param origem
+	 * @param aeroporto
+	 * @param rota
+	 */
 	public void addAresta(Aeroporto origem, Aeroporto aeroporto, Rota rota) {
 		int origemIndex = this.findAirportIndex(origem.getCodigo());
 		
+		//Se o aeroporto de origem não existe na L.V o adiciona na L.V e 
+		//adciona uma lista na L.A
 		if(origemIndex < 0){
 			lv.add(origem);
 			la.add(new LinkedList<>());
 			origemIndex = la.size()-1;
 		}
-		
+		//Adiciona aeroporto destino na lista do aeroporto origem
 		la.get(origemIndex).add(new Node(rota, aeroporto));
 	}
 		
+	/**
+	 * Obtem grau de saida de um nodo
+	 * @param nodo
+	 * @return
+	 */
 	public int grauDeSaida(int nodo){
 		return  la.get(nodo).size();
 	}
@@ -97,88 +134,22 @@ public class Grafo {
 		return cont;
 	}
 	
-	/*public int getAdjacenteProximo(int nodo){
-		if(la.get(nodo).size() == 0){
-			return -1;
-		}
-		int vDest = la.get(nodo).get(0).vDest;
-		int menorPeso = la.get(nodo).get(0).peso;
-		for(int i = 0; i < la.get(nodo).size(); i++){
-			if(menorPeso > la.get(nodo).get(i).peso){
-				menorPeso = la.get(nodo).get(i).peso;
-				vDest = la.get(nodo).get(i).vDest;
-			}
-		}
-		return vDest;
-	}*/
+	/**
+	 * Obtem a menor rota entre dois aeroportos
+	 * @param codigo1
+	 * @param codigo2
+	 */
+	public void determinarRotaDeMenorCusto(String codigo1, String codigo2){
+		
+	}
 	
-	//	
-//	public List<Integer> maiorGrau(){
-//		List<Integer> graus = new ArrayList<>();
-//		int maiorGrau = 0;
-//		for (int i = 0; i < ma.length; i++) {
-//			int grau = grau(i);
-//			if(grau > maiorGrau){
-//				graus = new ArrayList<>();
-//				maiorGrau = grau;
-//				graus.add(i);
-//			}else if (grau == maiorGrau){
-//				graus.add(i);
-//			}
-//		}		
-//		return graus;
-//	}
-//	
-//	//percorre abaixo da diagonal principal
-//	public int contAresta(){
-//		int i, j;
-//		int numAresta = 0;
-//		for (i=1; i<ma.length; i++) {
-//			for (j=0; j<=i; j++) {
-//				if (ma[i][j] == 1)
-//					numAresta++;
-//			}
-//			
-//		}
-//		return numAresta;
-//	}
-//	
-//	public ArrayList<Integer> percursoAmplitude(int nodoInicial) {
-//		ArrayList<Integer> listaAmplitude = new ArrayList<>();
-//		ArrayList<Integer> fila = new ArrayList<>();
-//		
-//		fila.add(nodoInicial);
-//		listaAmplitude.add(nodoInicial);
-//		while(!fila.isEmpty()){
-//			int nodo = fila.remove(0);		
-//			for (int i = 0; i < ma.length; i++) {
-//				if(ma[nodo][i] == 1){
-//					if(!listaAmplitude.contains(i)){
-//						fila.add(i);
-//						listaAmplitude.add(i);
-//					}
-//				}
-//			}
-//		}
-//		return listaAmplitude;
-//	}
-//	
-//	public List<Integer> percursoProfundidade(int nodoInicial) {
-//		List<Integer> lista = new ArrayList<>();
-//		percursoProfundidade(nodoInicial, lista);
-//		return lista;
-//	}
-//
-//	private void percursoProfundidade(int nodo, List<Integer> percurso) {
-//		percurso.add(nodo);
-//		for (int i = 0; i < ma.length; i++) {
-//			if (ma[nodo][i] != 0) {
-//				if (!percurso.contains(i)) { // Evita ciclos!
-//					percursoProfundidade(i, percurso);
-//				}
-//			}
-//		}
-//	}
+	/**
+	 * Verifica se uma cia aérea faz uma rota exclusiva
+	 * @param ciaNome
+	 */
+	public void verificarRotaExclusiva(String ciaNome){
+		
+	}
 	
 	@Override
 	public String toString() {
--- alged-t2/src/seiLa/App.java
+++ alged-t2/src/seiLa/App.java
@@ -3,6 +3,8 @@ package seiLa;
 import leitura.LeituraCiaAerea;
 import leitura.LeituraGeral;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 import Model.Aeroporto;
 import Model.CiaAerea;
@@ -12,25 +14,134 @@ import Model.Rota;
 
 public class App{
 	
+	private static Grafo grafo;
+
 	public static void main(String args[]){
-	LeituraGeral lg = new LeituraGeral();
-	
-	System.out.println("Lendo arquivos ... ");
-	
-	ArrayList<Pais> paises = lg.getLp();
-	ArrayList<CiaAerea> cias = lg.getLca();
-	ArrayList<Aeroporto> aeroportos = lg.getLa(paises);
-	ArrayList<Rota> rotas = lg.getLr(aeroportos,cias);
-	
-	System.out.println("Construindo grafo ... ");
-	Grafo grafo = new Grafo(rotas);
-	
-	System.out.println(grafo.grauDeSaida(1));
-	
-	//System.out.println(paises.toString());
-	//System.out.println(cias.toString());
-	//System.out.println(aeroportos.toString());
-	//System.out.println(rotas.toString());
-	
+		try{
+			LeituraGeral lg = new LeituraGeral();
+
+			System.out.println("Lendo arquivos ... ");
+
+			ArrayList<Pais> paises = lg.getLp();
+			ArrayList<CiaAerea> cias = lg.getLca();
+			ArrayList<Aeroporto> aeroportos = lg.getLa(paises);
+			ArrayList<Rota> rotas = lg.getLr(aeroportos,cias);
+
+			System.out.println("Construindo grafo ... ");
+			grafo = new Grafo(rotas);
+
+			//System.out.println(paises.toString());
+			//System.out.println(cias.toString());
+			//System.out.println(aeroportos.toString());
+			//System.out.println(rotas.toString());
+
+			Scanner scanner = new Scanner(System.in);
+			int opt = 1;
+
+			while (opt != 0) {
+				System.out.println("Digite: " + "\n 1- Para encontrar a menor rota entre dois aeroportos"
+						+ "\n 2- Para procurar uma rota exclusiva de uma companhia aérea" 
+						+ "\n 3- Para verificar a autonomia de voo de uma avião"
+						+ "\n 4- Para verificar a probabilidade de congestionamento de um aeroporto");
+		
+				opt = scanner.nextInt();
+
+				switch (opt) {
+				case 1:
+					determinarRotaDeMenorCusto();;
+					break;
+				case 2:
+					verificarRotaExclusiva();;
+					break;
+				case 3:
+					verificarAutonomiaDeVoo();;
+					break;
+				case 4:
+					verificarProbabilidadeDeCongestionamento();;
+					break;			
+				default:
+					System.out.println("Opção inválida.");
+					break;
+				}
+			}
+
+		} catch (InputMismatchException e) {
+			System.out.println("Opção inválida.\nEncerrando.");
+		} catch (Exception e) {
+			System.out.println("\nOcorreu um erro:\n" + e.getMessage() + "\n");
+		}
+
 	}
+
+	/**
+	 * Determinar a rota de menor custo, em termos de distância total,
+	 * entre dois aeroportos fornecidos pelo usuário (considerar aeroportos e
+	 * rotas apenas no mesmo país). Quais os nomes das companhias aéreas
+	 * que realizam essa rota.
+	 */
+	public static void determinarRotaDeMenorCusto(){
+		try{
+			Scanner s = new Scanner(System.in);
+			System.out.println("\nInforme o código do primeiro aeroporto:\n");
+			String id1 = s.next();
+			System.out.println("\nInforme o código do segundo aeroporto:\n");
+			String id2 = s.next();
+			grafo.determinarRotaDeMenorCusto(id1,id2);
+			
+		} catch (InputMismatchException e) {
+			System.out.println("\nInformação inválida:\n");
+		} catch (Exception e) {
+			System.out.println("\nOcorreu um erro:\n" + e.getMessage() + "\n");
+		}
+	}
+
+	/**
+	 * Fornecer o nome de uma companhia aérea e verificar se ela realiza
+	 * uma rota exclusiva entre dois aeroportos. Se existir, informar a rota
+	 * com as conexões.
+	 */
+	public static void verificarRotaExclusiva(){
+		try{
+			Scanner s = new Scanner(System.in);
+			System.out.println("\nInforme o nome de uma companhia aérea:\n");
+			String nome = s.next();
+			
+			grafo.verificarRotaExclusiva(nome);
+		} catch (InputMismatchException e) {
+			System.out.println("\nInformação inválida:\n");
+		} catch (Exception e) {
+			System.out.println("\nOcorreu um erro:\n" + e.getMessage() + "\n");
+		}
+	}
+
+	/**
+	 * Uma companhia deseja adquirir um avião de médio porte, com uma
+	 * determinada autonomia de voo (fornecida em Km). A tomada de
+	 * decisão da companhia está baseada no percentual de suas rotas onde
+	 * poderá utilizar esse avião. Se esse percentual for superior a 70%,
+	 * então a compra está autorizada. Fornecer a autonomia de voo de um
+	 * avião e informar se a compra será realizada ou não.
+	 */
+	public static void verificarAutonomiaDeVoo(){
+		/**
+		 * TODO pedir o nome da companhia que vai comprar o avião
+		 * e pedir a autonomia de voo desse aviao, então fazer a 
+		 * mágica para descobrir se esta companhia deve ou não
+		 * comprar o avião		 
+		 */
+	}
+
+	/**
+	 * Qual o aeroporto, de um determinado país, que possui a maior
+	 * probabilidade de congestionamento no futuro? Como medida utiliza-se
+	 * o número total de voos que chegam no aeroporto.
+	 */
+	public static void verificarProbabilidadeDeCongestionamento(){
+		/**
+		 * TODO pedir o país e verificar o maior grau de entrada de todos
+		 * os aeroportos desse país, o que tiver o maior grau é o que tem 
+		 * a maior probabilidade de ter congestionamento
+		 */
+	}
+
 }
